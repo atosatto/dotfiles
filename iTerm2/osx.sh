592a31f4dfae09af9e8737f5d46d973afaf1f9bb
Andrea Tosatto <andrea.tosy@gmail.com>
2014-12-31 11:17:52 +0100

    Added Solarized Themes for iTerm2 and GO settings

--- iTerm2/osx.sh
+++ iTerm2/osx.sh
@@ -6,6 +6,14 @@ echo "  Installing iTerm2 Colors."
 
 killall cfprefsd
 
+# Custom 'Solarized Dark' Theme
 /usr/libexec/PlistBuddy -c "Add :'Custom Color Presets':'Hilbert Solarized Dark' dict" ~/Library/Preferences/com.googlecode.iterm2.plist
 /usr/libexec/PlistBuddy -c "Merge 'hilbert-solarized.dark.itermcolors' :'Custom Color Presets':'Hilbert Solarized Dark'" ~/Library/Preferences/com.googlecode.iterm2.plist
 
+# Standard 'Solarized Dark' Theme
+/usr/libexec/PlistBuddy -c "Add :'Custom Color Presets':'Solarized Dark' dict" ~/Library/Preferences/com.googlecode.iterm2.plist
+/usr/libexec/PlistBuddy -c "Merge 'Solarized Dark.itermcolors' :'Custom Color Presets':'Solarized Dark'" ~/Library/Preferences/com.googlecode.iterm2.plist
+
+# Standard 'Solarized Light' Theme
+/usr/libexec/PlistBuddy -c "Add :'Custom Color Presets':'Solarized Light' dict" ~/Library/Preferences/com.googlecode.iterm2.plist
+/usr/libexec/PlistBuddy -c "Merge 'Solarized Light.itermcolors' :'Custom Color Presets':'Solarized Light'" ~/Library/Preferences/com.googlecode.iterm2.plist
