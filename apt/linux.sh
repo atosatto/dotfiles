20b91a98ec7b23e86f0ab8352e560b79d40e39d0
Andrea Tosatto <andrea.tosy@gmail.com>
2015-02-10 15:27:11 +0100

    More software packages

--- apt/linux.sh
+++ apt/linux.sh
@@ -10,30 +10,38 @@ set -e
 cd "$(dirname "$0")"
 
 # Extra Repos
-REPOS=
+REPOS=(
+	ppa:webupd8team/java
+)
 
 # In-repo packages
 PACKAGES=(
 		youtube-dl
-		git
-		git-flow
-    svn
+		vim
 		wget
 		nmap
+		git
+		git-flow
+		subversion
 		python
 		python3
 		nodejs
     ruby
-    virtualbox
+		oracle-java8-installer
+		ansible
+		virtualbox
+		network-manager-openvpn network-manager-openvpn-gnome
+		nfs-kernel-server
 )
 
 # On-web packages
 DEBS=(
-		https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb	# google-chrome
+		https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb 	# google-chrome
 		https://atom.io/download/deb	# atom
-		https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_1.6.2_amd64.deb	# dropbox
+		https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_1.6.2_amd64.deb	 # dropbox
 		http://download.skype.com/linux/skype-ubuntu-precise_4.3.0.37-1_i386.deb	# skype
     https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.2_x86_64.deb  # vagrant
+		http://downloads.typesafe.com/scala/2.11.5/scala-2.11.5.deb?_ga=1.15839732.331535665.1423576780 # scala
 )
 
 # Script requirements
@@ -62,7 +70,7 @@ do
     CMD="sudo dpkg -i $FILE"
     if ! $CMD;
     then
-        sudo apt-get install -f
+        sudo apt-get install -fy
         $CMD
     fi
     rm $FILE
