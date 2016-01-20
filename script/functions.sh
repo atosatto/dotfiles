2abeeec990c8ed0e9e0845d51f0d1bc3c891c0f4
Andrea Tosatto <andrea.tosy@gmail.com>
2016-01-20 10:19:04 +0100

    Fixing parse errors in functions.sh

--- script/functions.sh
+++ script/functions.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# bootstrap installs things.
+# common functions
 
 set -e
 
@@ -29,53 +29,51 @@ link_files () {
 
 install_dotfiles () {
 
-  src=$1
-  do
-    dest="$HOME/.`basename \"${src%.*}\"`"
+  src="$1"
+  dest="$HOME/.`basename \"${src%.*}\"`"
 
-    if [ -f $dest ] || [ -d $dest ]
+  if [ -f $dest ] || [ -d $dest ]
+  then
+
+    overwrite=false
+    backup=false
+    skip=false
+
+    user "File already exists: `basename $src`, what do you want to do? [s]kip, [o]verwrite, [b]ackup?"
+    read -n 1 action
+
+    case "$action" in
+      o )
+        overwrite=true;;
+      b )
+        backup=true;;
+      s )
+        skip=true;;
+      * )
+        ;;
+    esac
+
+    if [ "$overwrite" == "true" ]
     then
-
-      overwrite=false
-      backup=false
-      skip=false
-
-      user "File already exists: `basename $src`, what do you want to do? [s]kip, [o]verwrite, [b]ackup?"
-      read -n 1 action
-
-      case "$action" in
-        o )
-          overwrite=true;;
-        b )
-          backup=true;;
-        s )
-          skip=true;;
-        * )
-          ;;
-      esac
-
-      if [ "$overwrite" == "true" ]
-      then
-        rm -rf $dest
-        success "removed $dest"
-      fi
-
-      if [ "$backup" == "true" ]
-      then
-        mv $dest $dest\.backup
-        success "moved $dest to $dest.backup"
-      fi
-
-      if [ "$skip" == "false" ]
-      then
-        link_files $src $dest
-      else
-        success "skipped $src"
-      fi
-
-    else
-      link_files $src $dest
+      rm -rf $dest
+      success "removed $dest"
     fi
 
-  done
+    if [ "$backup" == "true" ]
+    then
+      mv $dest $dest\.backup
+      success "moved $dest to $dest.backup"
+    fi
+
+    if [ "$skip" == "false" ]
+    then
+      link_files $src $dest
+    else
+      success "skipped $src"
+    fi
+
+  else
+    link_files $src $dest
+  fi
+
 }
