180ae013eb4c6e6698871609bbc4ad9df48e5e1d
Andrea Tosatto <andrea.tosy@gmail.com>
2019-09-05 17:45:07 +0200

    python: automatically install python with pyenv

--- python/all.sh
+++ python/all.sh
@@ -5,11 +5,27 @@ source "scripts/functions.sh"
 
 title "Setup Python"
 
+VERSION_GLOBAL="3.7.4"
+
+VERSIONS=(
+  "3.6.9"
+  "$VERSION_GLOBAL"
+)
+
 PACKAGES=(
   ansible
   molecule
   neovim
 )
+
+for version in "${VERSIONS[@]}"
+do
+  info "Installing Python $version"
+  pyenv install -s $version
+done
+
+info "Setting Python $VERSION_GLOBAL as default Python version"
+pyenv global $VERSION_GLOBAL
 
 for package in "${PACKAGES[@]}"
 do
